3f6a5e83fda5f3aab2a1894e197942097b637ac2
dcardezo <102082998+dcardezo@users.noreply.github.com>
2025-04-05 12:55:58 +0200

    [t1][.json, project_template] Add necessary folders, add tasks.json, adapt workspace for compilation, create an init cpp file example

---
+++ project_template/project_template.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Default template" << endl;
+    system("pause"); // Espera a que el usuario presione Enter
+    return 0;
+}
